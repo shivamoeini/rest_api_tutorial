0fd2bc7f8d16498eeca4ddc06e9a09d80d4daadf
shiva moeini <shivamoeini2000@gmail.cpm>
2022-08-11 23:16:07 +0430

    cmit

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports={
+   MOUNGO_URI: "mongodb+srv://admin:shiva79@cluster0.u4rtgjl.mongodb.net/myapp?retryWrites=true&w=majority"
+}
--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 const express=require('express');
+const mongoose =require('mongoose');
+const{ MOUNGO_URI}=require('./config')
+const app=express();
+// connect to mongodb
+mongoose.connect( MOUNGO_URI,{
+    useNewUrlparser:true,
+    useUnifiedTopology:true,
+})
+.then(()=>console.log("mongoDB connected!"))
+.catch(err=>console.log(err));
+const PORT=process.env.PORT || 5000;
+app.listen(PORT,()=>console.log(`server run at port ${PORT} `))
