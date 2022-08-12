c22e2c9a06d74fee26594ec654f68bb9ff3bb531
shiva moeini <shivamoeini2000@gmail.cpm>
2022-08-12 21:34:59 +0430

    save

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,8 +5,16 @@ const Posts=require("../../models/posts");
 //@routes post api/posts
 // @desc create an post
 
-router.post('/',(req,res)=>{
-  console.log(req.body);
+router.post('/',async(req,res)=>{
+
+    const newPost=new Posts(req.body);
+    try{
+  const post=await newPost.save();
+        if(!post) throw Error("Something went wron while saveing the post");
+        res.status(200).json(post);
+    }catch(err){
+        res.status(400).json({msg:err})
+    }
 
 });
 module.exports=router;
